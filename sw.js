3f42ccee7498ba9fb59e6e24f15b1a41a159af62
Hans <c0989352389@gmail.com>
2026-05-12 18:24:51 +0800

    bump: SW cache v3→v4 強制更新所有資源,清掉手機端可能的舊版快取

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'song-v3';
+const CACHE = 'song-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', e => {
