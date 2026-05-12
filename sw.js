5dfed524daf161ef86861d6f3c4c40cbad28225d
Hans <c0989352389@gmail.com>
2026-05-13 02:28:03 +0800

    chore: bump sw cache v5→v6 (force icon refresh)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'song-v5';
+const CACHE = 'song-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', e => {
